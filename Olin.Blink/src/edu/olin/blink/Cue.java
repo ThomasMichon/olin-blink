60c65812cb4b9f49838177c83e28af6dd80d13f0
tbmichon <tbmichon@791d665e-bf27-0410-abb3-b3e4516476d0>
2007-01-24 02:20:36 +0000

    Made a default implementation.

--- Olin.Blink/src/edu/olin/blink/Cue.java
+++ Olin.Blink/src/edu/olin/blink/Cue.java
@@ -2,9 +2,36 @@ package edu.olin.blink;
 
 public class Cue {
 	
-	public Cue() {
-            //hey I modified this
-            // -jeff
+	private int time;
+	private int level;
+	
+	/**
+	 *	Creates a <tt>Cue</tt>.
+	 *
+	 *	@param time the time at which this cue must be executed (milliseconds).
+	 *	@param level the level that the output must be set to.
+	 */
+	public Cue(int time, int level) {
+		this.time = time;
+		this.level = level;
+	}
+	
+	/**
+	 *	Retrieves the time that this cue is scheduled to go off, relative to the some beginning.
+	 *
+	 *	@return a time value in milliseconds.
+	 */
+	public int getTime() {
+		return this.time;
+	}
+	
+	/**
+	 * Retrieves the level that the associated channel must set it's output to.
+	 *
+	 * @return a level value between 0 and 255.
+	 */
+	public int getLevel() {
+		return this.level;
 	}
 	
 }
